73413cacc2cc0ba16882b0a752011fb3cf3c4d1e
czy <zhiyuan2.21@intl.zju.edu.cn>
2022-05-24 19:27:29 +0800

    add lable and improve pseudocode

--- Practical6/health.py
+++ Practical6/health.py
@@ -2,7 +2,7 @@
 # when we were born. In particular, it seems that health problems increase in frequency with the
 # father’s age at the time of the offspring birth.
 a=input('please enter your father ages:') #input the age of the father
-paternal_age=[30,35,40,45,50,55,60,65,70,75]
+paternal_age=[30,35,40,45,50,55,60,65,70,75] # the original data
 age_risk=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
 dictionary=dict(zip(paternal_age,age_risk)) #combine into a dictionary
 print(dictionary)
@@ -12,6 +12,9 @@ draw the scatter plot below
 '''
 import matplotlib.pyplot as plt
 N=10
+plt.title('the scatter plot for the health risk related to the father age when born')
+plt.xlabel('father age')
+plt.ylabel('the health risk')
 x=paternal_age
 y=age_risk
 plt.scatter(x,y,N)
